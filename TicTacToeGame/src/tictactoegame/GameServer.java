75608a5efe304d6eff5e2e74c986f7ded7996669
HeBa <heba.Ibrahem.x@gmail.com>
2020-02-12 14:54:20 -0500

    Add files via upload
    //Handlers for each connection added
    //Error line 95 
    Using user-defined child from Socket Class approach

--- TicTacToeGame/src/tictactoegame/GameServer.java
+++ TicTacToeGame/src/tictactoegame/GameServer.java
@@ -5,7 +5,9 @@
 package projectserver;
 
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -18,20 +20,14 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.util.Vector;
-import static javafx.application.Application.launch;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 
 /**
  *
  * @author BOB
  */
 public class GameServer extends Application {
-    
-    public void init() throws IOException
-    {
   
-    }
-    
     
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -80,8 +76,9 @@ public class GameServer extends Application {
     {
         
         ServerSocket server = new ServerSocket(9000);//creating a socket and binding it to a port with loopback ip
-        Vector  clients_sockets= new Vector();
-        
+      
+       XO c;
+        Socket client;
         public Background() throws IOException {
         
      }
@@ -92,23 +89,74 @@ public class GameServer extends Application {
             System.out.println("server is listening");
              while(true){
                  try {
-                     clients_sockets.add(server.accept());
+                      
+                     client= (Socket)c;
+                     client = server.accept();
+                     //c=client;//????
+                     ClientHandlerThread x = new ClientHandlerThread(c);
+                     System.out.println("a client connected");
+                    
                      
-                     
-                 } catch (IOException ex) {
+                } catch (IOException ex) {
                      Logger.getLogger(GameServer.class.getName()).log(Level.SEVERE, null, ex);
                  }
           }
           
         
     }}
+    
+    public class ClientHandlerThread extends Thread
+    
+    {
+    /*To open a thread for every client connects */
+         DataInputStream receiveOn;
+         PrintStream sendOn;
+           Vector <XO> clients_sockets= new Vector();
+
+        public ClientHandlerThread(XO c) throws IOException {
+           clients_sockets.add(c);
+           receiveOn= new DataInputStream(c.getInputStream());
+           sendOn = new PrintStream(c.getOutputStream());
+       
+        }
+        
+        public void run()
+        {
+        while(true)
+        {
+            String opponent;
+            try {
+   opponent=   receiveOn.readLine();
+                for(XO p :clients_sockets)
+                {
+                /* this loop shall iterate over client sockets and find the suitable one get his outputstream 
+                 * and send to him the what ever
+                 */
+                    if (p.getPlayer()==opponent)
+                    { 
+                      DataInputStream rev =new DataInputStream(p.getInputStream());
+         PrintStream sen = new PrintStream(p.getOutputStream());
+         String msg =receiveOn.readLine();
+           sen.println(msg);
+                }
+                    System.out.println(opponent);
+               
+                } } catch (IOException ex) {
+                Logger.getLogger(GameServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        }
+         
+         
+    
+    }
     public static void main(String[] args) {
        
-           Thread curr= Thread.currentThread();
-                System.out.println(curr);
-                  int noOfThreads= Thread.activeCount();
-                     System.out.println(noOfThreads);
-            launch(args);
-               System.out.println(noOfThreads);
+           //Thread curr= Thread.currentThread();
+                //System.out.println(curr);
+                 // int noOfThreads= Thread.activeCount();
+                //     System.out.println(noOfThreads);
+       Application.launch(args);
+              // System.out.println(noOfThreads);
     }
 }
