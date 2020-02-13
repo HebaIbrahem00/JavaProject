16685f5ccb4996686d3d522bf0bc7a3e3189adb6
HeBa <heba.Ibrahem.x@gmail.com>
2020-02-13 18:18:53 +0200

    Add files via upload

---
+++ TicTacToeGame/src/tictactoegame/TestClient.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamev2;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author BOB
+ */
+public class TestClient {
+     public static void main(String[] args) throws UnknownHostException, IOException {
+   
+   CustomizedClientSocket s  =new CustomizedClientSocket("127.0.0.1",9000,"Ahmed");
+  
+while(true)
+{
+String msgfrompeer =s.fromServer.readLine();
+System.out.println("kda waslt 3and Ahmed "+msgfrompeer);
+}
+   
+   }
+    
+}
