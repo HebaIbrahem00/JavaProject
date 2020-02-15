9d0c56e9e27607be0905a261ed4cc967d488d10f
mofath <58465052+mofath@users.noreply.github.com>
2020-02-15 16:52:29 +0200

    Update MainApp.java

--- TicTacToeGame/JavaProject/src/MainApp.java
+++ TicTacToeGame/JavaProject/src/MainApp.java
@@ -1,17 +1,8 @@
 
-import Database.User_DB;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 class MainApp {
 
     public static void main(String[] args) {
         //run the first screen (login screen) in the game
         new LoginScreen();
-
-
     }
 }
