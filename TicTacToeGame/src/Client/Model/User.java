aa4c9cba07131bc504314ece8076ccac5bb6f1ed
Omaure <omarakram958@gmail.com>
2020-02-15 18:12:13 +0200

    Editing Database and Integrating

---
+++ TicTacToeGame/src/Client/Model/User.java
@@ -0,0 +1,71 @@
+package Client.Model;
+
+import javafx.beans.property.*;
+import java.sql.Date;
+
+public class User {
+
+    //Declare user Table Columns
+
+    private int userID;
+    private String userName;
+    private String password;
+    private String email;
+    private String userStatus;
+
+    //constructor
+    public User(int userID, String userName, String password, String email, String userStatus) {
+        this.userID = userID;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.userStatus = userStatus;
+    }
+
+    public User(String userName, String userStatus) {
+        this.userName = userName;
+        this.userStatus = userStatus;
+    }
+
+    //setter methods
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    //getter methods
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getUserStatus() {
+        return this.userStatus;
+    }
+}
--- TicTacToeGame/src/Server/GameServer.java
+++ TicTacToeGame/src/Server/GameServer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictactoegame;
+package Server;
 
 import java.io.DataInputStream;
 import java.io.IOException;
