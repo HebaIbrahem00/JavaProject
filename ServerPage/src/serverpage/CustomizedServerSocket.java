c112fc5a7100138babbeb09b4c11684a875ea26f
Omaure <omarakram958@gmail.com>
2020-02-23 02:04:29 +0200

    Merge Server to GUI

---
+++ ServerPage/src/serverpage/CustomizedServerSocket.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverpage;
+
+import java.net.SocketImpl ;
+
+import java.lang.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.*;
+
+/**
+ *
+ * @author BOB
+ */
+public class CustomizedServerSocket extends ServerSocket{
+
+   
+    public CustomizedServerSocket(int port) throws IOException {
+        super(port);
+    }
+    public Socket accept() throws IOException {
+        Socket s;
+        
+        s = new CustomizedClientSocket((SocketImpl) null);
+        implAccept(s);
+        
+        return s;
+    }
+
+    
+}
